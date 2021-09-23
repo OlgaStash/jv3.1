5af09c134b851254c5dfd3d5a4b326c735a45261
Olga Stashenyuk <olgastachenyk@mail.ru>
2021-09-23 16:58:49 +0300

    changes classes 2

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class BonusMilesService {
+public class BonusMilesService {
+    public int calculate(int price) {
+        int oneMile = 20;
+        int miles = price / oneMile;
+        return miles;
+    }
 }
